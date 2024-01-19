e06d64b837e3a3e73e76ec340c7f4528b842cfbe
ecnu-Sun <144778463+ecnu-Sun@users.noreply.github.com>
2024-01-19 13:56:16 +0800

    Update 数据预处理第二阶段.py

--- final/数据预处理第二阶段.py
+++ final/数据预处理第二阶段.py
@@ -11,7 +11,7 @@ from sklearn.decomposition import PCA
 
 '''
 df = pd.read_csv('processed_data_phase1.csv', na_values="NA", parse_dates=['Date'])
-# 单独统计每个字段的缺失值数量
+# 统计每个字段的缺失值数量
 evaporation_missing = df['Evaporation'].isna().sum()
 sunshine_missing = df['Sunshine'].isna().sum()
 
@@ -24,10 +24,9 @@ print(f"同时缺失 Evaporation 和 Sunshine 的行数: {both_missing}")
 
 def convert_to_tuple(s):
     try:
-        # 使用literal_eval来解析字符串
+        # 解析字符串
         return ast.literal_eval(s)
     except (ValueError, SyntaxError):
-        # 如果解析失败，返回原始字符串
         return s
 
 
@@ -47,20 +46,18 @@ df_complete['WindDir3pm_Y'] = df_complete['WindDir3pm'].apply(lambda x: x[1])
 # 'WindDirX' 和 'WindDirY' 是风向的正弦和余弦分量
 numeric_cols = ['WindGustDir_X', 'WindGustDir_Y', 'WindDir9am_X', 'WindDir9am_Y', 'WindDir3pm_X', 'WindDir3pm_Y'] \
                + df.select_dtypes(include=[np.number]).columns.tolist()
-
-# 从完整数据中选择这些列
 df_numeric = df_complete[numeric_cols]
 
-# 4. 执行 PCA
+# PCA
 pca = PCA(n_components=len(numeric_cols))
 pca.fit(df_numeric)
 
-# 计算主成分和原始特征之间的相关性
+# 主成分和原始特征之间的相关性
 components_corr = pd.DataFrame(pca.components_, columns=numeric_cols)
 
 explained_var_ratio = pca.explained_variance_ratio_
 cumulative_var_ratio = np.cumsum(pca.explained_variance_ratio_)
-# 创建一个条形图来显示每个主成分的方差比例
+# 显示每个主成分的方差比例
 plt.figure(figsize=(15, 8))
 plt.bar(range(1, len(pca.explained_variance_ratio_) + 1), pca.explained_variance_ratio_, alpha=0.5, align='center')
 plt.step(range(1, len(np.cumsum(pca.explained_variance_ratio_)) + 1), np.cumsum(pca.explained_variance_ratio_), where='mid')
@@ -69,7 +66,7 @@ plt.xlabel('Principal components')
 
 for i, component in enumerate(pca.components_):
     most_contributing_feature = numeric_cols[np.argmax(np.abs(component))]
-    y_position = pca.explained_variance_ratio_[i] + 0.02  # 计算y坐标位置
+    y_position = pca.explained_variance_ratio_[i] + 0.02  
     x_position = i + 1 - 0.1
     plt.annotate(f"PC{i+1}-{most_contributing_feature}",
                  xy=(i + 1, pca.explained_variance_ratio_[i]),
@@ -81,19 +78,14 @@ for i, component in enumerate(pca.components_):
 plt.title('Explained Variance of Principal Components')
 plt.show()
 
-# 计算相关性矩阵
+# 计算相关性矩阵 创建热力图
 corr_matrix = df_numeric.corr()
-
-# 创建一个热力图
 plt.figure(figsize=(12, 8))
 sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap='coolwarm')
-
-# 显示热力图
 plt.show()
 exit(0)
-# 删除 Sunshine 缺失的行
+# 删除 Sunshine 缺失的行 删除风向特征
 df = df.dropna(subset=['Sunshine'])
-# 删除风向列
 df = df.drop(columns=['WindGustDir', 'WindDir9am', 'WindDir3pm'])
 df.to_csv('processed_data_phase2.csv', index=False)
 
